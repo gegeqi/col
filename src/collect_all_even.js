e58b58095aac86eaba32449948b22fcaf4316a3c
qigege <ccczzz1015@163.com>
2016-05-31 13:36:01 +0800

    test

--- src/collect_all_even.js
+++ src/collect_all_even.js
@@ -1,15 +1,15 @@
 
-function collect_same_elements(collection_a, object_b) {
-	var collection_a=new Array();
-	var collection_b=new Array();
-	for(var i=0;i<collection_a.length;i++){
-
- var arr_a=collection_a[i].key;
-for(var j=0;j<collection_b.value.length;j++){
-   var arr_b=collection_b.value[j];
-       if(collection_a[i].key==collection_b.value[j]){
-           alert(collection_a[i].key)
-}
-}
-}
+function collect_same_elements(collection_a, object_b) {	
+var arr=new Array();
+var m=0;
+  for(var i=0; i<collection_a.length; i++){
+       for(var  j=0 ; j<object_b.value.length ; j++){
+              if(collection_a[i].key==object_b.value[j]){                
+                 arr[m] = collection_a[i].key;
+                 m++;
+          }
+     }
+  } 
+  return arr;
+}    
 module.exports = collect_same_elements;
